97e8993ba03309392139c976c03926b72d0e2436
JieWu <ngjiewu@gmail.com>
2019-08-28 11:39:28 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,15 +10,22 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
 
+        String[] tasks = new String[100];
+        int counter = 0;
         while (true) {
             Scanner in = new Scanner(System.in);
             String command = in.nextLine();
-
-            if (!command.equals("bye")) {
-                System.out.println(command);
-            } else {
+            if (command.equals("list")) {
+                for (int i = 0; tasks[i] != null; i++) {
+                    System.out.println(i+1 + ". " + tasks[i]);
+                }
+            } else if (command.equals("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else {
+                tasks[counter] = command;
+                System.out.println("added: " + command);
+                counter++;
             }
         }
     }
